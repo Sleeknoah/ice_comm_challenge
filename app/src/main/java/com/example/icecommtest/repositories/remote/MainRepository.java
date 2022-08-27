5b61acea16599ad5d6611729d15df324665a85fe
Sleeknoah <yungdyke@gmail.com>
2022-08-27 02:37:40 +0100

    5.0.0 Product Fragment implementation (Code sanitation)

--- app/src/main/java/com/example/icecommtest/repositories/remote/MainRepository.java
+++ app/src/main/java/com/example/icecommtest/repositories/remote/MainRepository.java
@@ -1,49 +1,28 @@
 package com.example.icecommtest.repositories.remote;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
-import com.example.icecommtest.model.response.LoginResponse;
 import com.example.icecommtest.model.response.ProductResponse;
-import com.example.icecommtest.model.response.SignUpResponse;
 import com.example.icecommtest.utils.ApiHelper;
-import com.example.icecommtest.utils.BaseUrl;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainRepository {
     public static MutableLiveData<List<ProductResponse>> productResponseMutableLiveData = new MutableLiveData<>();
     public static  MutableLiveData<String> messageMutableLiveData = new MutableLiveData<>();
 
-    ProductResponse productResponse = new ProductResponse();
-    private ApiInterface apiInterface;
-
-    ApiInterface authInterface(){
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BaseUrl.FAKE_API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        apiInterface = retrofit.create(ApiInterface.class);
-
-        return apiInterface;
-    }
-
     public void productByCategory(String category){
-        authInterface().productCategory(category).enqueue(new Callback<List<ProductResponse>>() {
+        ApiHelper.authInterface().productCategory(category)
+                .enqueue(new Callback<List<ProductResponse>>() {
             @Override
-            public void onResponse(@NonNull Call<List<ProductResponse>> call, @NonNull Response<List<ProductResponse>> response) {
+            public void onResponse(@NonNull Call<List<ProductResponse>> call,
+                                   @NonNull Response<List<ProductResponse>> response) {
                 if (response.isSuccessful()){
-                    messageMutableLiveData.postValue("success");
                     productResponseMutableLiveData.postValue(response.body());
                 }else {
                     messageMutableLiveData.postValue("error");
@@ -51,8 +30,9 @@ public class MainRepository {
             }
 
             @Override
-            public void onFailure(@NonNull Call<List<ProductResponse>> call, @NonNull Throwable t) {
-                Log.d("TAG", "onFailure: " + t.getMessage());
+            public void onFailure(@NonNull Call<List<ProductResponse>> call,
+                                  @NonNull Throwable t) {
+                messageMutableLiveData.postValue("error");
             }
         });
     }
--- app/src/main/java/com/example/icecommtest/view/fragments/ProductFragment.java
+++ app/src/main/java/com/example/icecommtest/view/fragments/ProductFragment.java
@@ -5,13 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -85,23 +83,29 @@ public class ProductFragment extends Fragment {
         //Livedata Observer is set here to observe change in the product response State
         LiveData<List<ProductResponse>> productObservable = mainViewModel.observeProductCategory();
 
-        productObservable.observe(getViewLifecycleOwner(), new Observer<List<ProductResponse>>() {
-            @Override
-            public void onChanged(List<ProductResponse> productResponse) {
-                if (productResponse != null){
-                    adapter = new ProductAdapter(requireActivity(), productResponse);
-                    // setting grid layout manager to implement grid view.
-                    // in this method '2' represents number of columns to be displayed in grid view.
-                    GridLayoutManager layoutManager=new GridLayoutManager(requireContext(),2);
-
-                    // at last set adapter to recycler view.
-                    binding.productRecycler.setLayoutManager(layoutManager);
-                    binding.productRecycler.setAdapter(adapter);
-
-                    binding.productRecycler.setVisibility(View.VISIBLE);
-
-                    productObservable.removeObservers(requireActivity());
+        mainViewModel.observeMessage().observe(getViewLifecycleOwner(), s -> {
+            if(s != null){
+                if (s.equals("error")){
+                    Toast.makeText(requireContext(), "Error loading products", Toast.LENGTH_SHORT).show();
                 }
+
+            }
+        });
+
+        productObservable.observe(getViewLifecycleOwner(), productResponse -> {
+            if (productResponse != null){
+                adapter = new ProductAdapter(requireActivity(), productResponse);
+                // setting grid layout manager to implement grid view.
+                // in this method '2' represents number of columns to be displayed in grid view.
+                GridLayoutManager layoutManager=new GridLayoutManager(requireContext(),2);
+
+                // at last set adapter to recycler view.
+                binding.productRecycler.setLayoutManager(layoutManager);
+                binding.productRecycler.setAdapter(adapter);
+
+                binding.productRecycler.setVisibility(View.VISIBLE);
+
+                productObservable.removeObservers(requireActivity());
             }
         });
     }
